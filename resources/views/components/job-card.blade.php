2f1075da22c773e5d61b96b606cc48d106245036
imtyaz-17 <imtyazit17017@gmail.com>
2024-06-03 16:38:15 +0600

    job-card added

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="self-start">
+        Laracast
+    </div>
+    <div class="text-center">
+        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Graphics Designer
+        </h3>
+        <p class="mb-3 font-bold text-gray-700 dark:text-gray-400">Full Time - $4000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img src="http://placehold.it/42/42" class="rounded-xl" alt="">
+    </div>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,2 @@
+<a href=""
+    class="p-2 bg-orange-200 hover:bg-black hover:text-white rounded-xl text-xs transition-colors duration-300">{{$slot}}</a>
